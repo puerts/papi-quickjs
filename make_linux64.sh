c55f574102edab3d8f1a709515c734f05c7e9e86
johnche <johnche@tencent.com>
2024-12-12 15:59:53 +0800

    部分编译错误

--- make_linux64.sh
+++ make_linux64.sh
@@ -1,4 +1,4 @@
 mkdir -p build_linux64 && cd build_linux64
-cmake-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ../
+cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ../
 cd ..
 cmake --build build_linux64 --config Release
