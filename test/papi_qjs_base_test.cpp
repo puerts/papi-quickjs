adb9b73ad9956c0c47ed4773adbc72b64f2b63b1
johnche <johnche@tencent.com>
2025-02-20 19:54:44 +0800

    添加注册api的测试

--- test/papi_qjs_base_test.cpp
+++ test/papi_qjs_base_test.cpp
@@ -1,11 +1,18 @@
 ﻿#include <gtest/gtest.h>
 #include "CppObjectMapper.h"
 #include <quickjs/quickjs.h>
+#include "pesapi.h"
+#include "JSClassRegister.h"
 
 namespace pesapi {
 namespace qjsimpl {
 
 class CppObjectMapperTest : public ::testing::Test {
+public:
+    static void Foo(struct pesapi_ffi* apis, pesapi_callback_info info)
+    {
+
+    }
 protected:
     void SetUp() override {
         env_ref = create_qjs_env();
@@ -26,6 +33,19 @@ TEST_F(CppObjectMapperTest, CreateAndDestroyMultQjsEnv) {
         pesapi_env_ref env_ref = create_qjs_env();
         destroy_qjs_env(env_ref);
     }
+}
+
+TEST_F(CppObjectMapperTest, RegApi) {
+    pesapi_property_descriptor properties = pesapi_alloc_property_descriptors(1);
+    pesapi_set_method_info(properties, 0, "Foo", true, Foo, NULL, NULL);
+    pesapi_define_class("Test", NULL, "Test", NULL, NULL, 1, properties, NULL);
+
+    auto clsDef = puerts::FindClassByID("Test");
+    ASSERT_TRUE(clsDef != nullptr);
+
+    ASSERT_TRUE(strcmp(clsDef->Functions[0].Name, "Foo") == 0);
+
+    ASSERT_TRUE(clsDef->Functions[0].Callback == Foo);
 }
 
 
