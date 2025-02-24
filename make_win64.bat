432118b31742f62d0c91582a655b815f805c2f68
johnche <johnche@tencent.com>
2025-02-24 13:13:11 +0800

    用debug版本测试

--- make_win64.bat
+++ make_win64.bat
@@ -1,11 +1,15 @@
-mkdir build & pushd build
+@echo off
+setlocal enabledelayedexpansion
 
-if "%1" == "1" (
-  cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DQJS_NS=1 -G "Visual Studio 16 2019" -A x64 ..
-) else (
-  cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 16 2019" -A x64 ..
+set CONFIG=Release
+if not "%1"=="" (
+    set CONFIG=%1
 )
 
+mkdir build & pushd build
+
+cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 16 2019" -A x64 ..
+
 popd
-cmake --build build --config Release
+cmake --build build --config %CONFIG%
 pause
